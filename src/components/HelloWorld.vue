492b165134a323698405db4340d6b116283f9945
maruiping <13833589337@163.com>
2020-04-04 16:16:54 +0800

    新增lodash依赖，使用_.indexOf和_.slice实现一行的多次合并，修复数据中有null值会合并数据的问题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,13 +9,24 @@ import { download } from "../../static/js/download";
 export default {
     name: "hello",
     data() {
-        return {};
+        return {
+            nullStart: 0, // null值开始的位置
+            nullList: [], // 所有null值的位置，用来合并使用
+            nullLast: -1, // 数组中null最后一次的位置
+            otherArr: [], // 相邻null位置统计
+            newArr: [] // 所有相邻null拼接成一个新数组
+        };
     },
     methods: {
         // 下载文件
         uploadFile() {
             var allNum = [
-                { 姓名: "张三", 性别: "男", 年龄: 19, 注册时间: "2019/10/10" },
+                {
+                    姓名: "张三",
+                    性别: "男",
+                    年龄: null,
+                    注册时间: "2019/10/10"
+                },
                 { 姓名: "李四", 性别: "女", 年龄: 19, 注册时间: "2019/12/12" },
                 { 姓名: "王五", 性别: "男", 年龄: 20, 注册时间: "2019/11/08" },
                 { 姓名: "赵六", 性别: "女", 年龄: 22, 注册时间: "2020/10/10" }
@@ -27,11 +38,11 @@ export default {
                 { 学号: "00004", 姓名: "赵六", 性别: "女", 分数: 55 }
             ];
             /**
-             * 注意，每行目前只能支持一次合并，合并列数不限
              * 再看看能不能判断每一个合并
+             * 一行可以进行多次合并 注意需要合并的内容置null
              */
             var aoa = [
-                ["主要信息", null, null, "其它信息"], // 特别注意合并的地方后面预留2个null
+                ["主要信息", null, null, "其它信息", null, "中间"], // 特别注意合并的地方后面预留null
                 ["测试行", null, null, null], // 试试合并到头
                 ["测试合并", "在中间", null, "会不会有问题"], // 试试合并在中间
                 ["测试合并", "在最后", "会不会有问题", null], // 试试尾部合并
@@ -40,8 +51,8 @@ export default {
             var aoa1 = [];
             download.openDownloadDialog(
                 download.sheet2blob([
-                    this.createSheet(allNum, aoa, "sheet1"),
-                    this.createSheet(allNum1, aoa1, "sheet2")
+                    this.createSheet(allNum, aoa, "sheet1")
+                    // this.createSheet(allNum1, aoa1, "sheet2")
                 ]), // 批量导出数据 sheetName要不同，不然会导出失败，报错
                 "导出.xlsx" // 导出表格的文件名
             );
@@ -55,6 +66,34 @@ export default {
             for (var i in data[0]) {
                 aoa[topDateNum].push(i); // 当你的表头与表格需要显示表头相同时可以遍历
             }
+            var hbLine = [];
+            aoa.map((item, index) => {
+                if (item.indexOf(null) > -1) {
+                    // 每次判断合并行的时候进行一次数据还原
+                    this.nullStart = 0;
+                    this.nullList = [];
+                    this.nullLast = -1;
+                    this.otherArr = [];
+                    this.newArr = [];
+                    this.isNull(item);
+                    this.isNext(this.nullList);
+                    for (var i in this.newArr) {
+                        // console.log(this.newArr[i])
+                        hbLine.push({
+                            // 设置某一行的单元格合并（每行只支持一次合并）
+                            // s:{r:num,c:num} 开始 ,e:{r:num,c:num} 结束
+                            // r:行 c:列
+                            s: { r: index, c: this.newArr[i][0] - 1 },
+                            e: {
+                                r: index,
+                                c: this.newArr[i][0] - 1 + this.newArr[i].length
+                            }
+                        });
+                    }
+                } else {
+                    return;
+                }
+            });
             data.map(item => {
                 aoa.push([
                     item[aoa[topDateNum][0]],
@@ -64,34 +103,30 @@ export default {
                 ]); // 当你的表头与表格需要显示表头相同时可以直接取表头的信息
             });
             var sheet = XLSX.utils.aoa_to_sheet(aoa);
-            var hbLine = [];
-            aoa.map((item, index) => {
-                if (item.indexOf(null) > -1) {
-                    var nullStart = item.indexOf(null);
-                    // 减一是因为，null值不是第一个，而null值前的那个值是要合并的第一个
-                    nullStart--;
-                    // var nullNum = 0;
-                    var nullNum = arraySiteNum(item, null);
-                    // for (var i of item) {
-                    //     if (i == null) {
-                    //         nullNum++;
-                    //     }
-                    // }
-                    hbLine.push({
-                        // 设置某一行的单元格合并（每行只支持一次合并）
-                        // s:{r:num,c:num} 开始 ,e:{r:num,c:num} 结束
-                        // r:行 c:列
-                        s: { r: index, c: nullStart },
-                        e: { r: index, c: nullNum + nullStart }
-                    });
-                } else {
-                    return;
-                }
-            });
-
             sheet["!merges"] = hbLine;
 
             return { sheetData: sheet, sheetName: sheetName };
+        },
+        // 把数据格式[1,2,3,5,6,7,9]=>[[1,2,3],[5,6,7],[9]]
+        isNext(arr) {
+            for (var i = 0; i < arr.length; i++) {
+                this.otherArr.push(arr[i]);
+                if (arr[i + 1] && arr[i + 1] - arr[i] == 1) {
+                    this.otherArr.push(arr[i + 1]);
+                } else {
+                    this.newArr.push(Array.from(new Set(this.otherArr)));
+                    this.otherArr = [];
+                }
+            }
+        },
+        // 循环调用isNull方法，判断所有的null值的位置
+        isNull(arr) {
+            if (_.indexOf(arr, null) > -1) {
+                this.nullStart = _.indexOf(arr, null);
+                this.nullLast += this.nullStart + 1;
+                this.nullList.push(this.nullLast);
+                this.isNull(_.slice(arr, this.nullStart + 1));
+            }
         }
     }
 };
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
+import lodash from 'lodash'
 Vue.config.productionTip = false
-
+Vue.prototype._ = lodash
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
